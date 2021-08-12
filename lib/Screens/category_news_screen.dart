f15819e3ab8b7961c094e0e9483e9cfc8e6a01d1
jungali10 <jungali1000@gmail.com>
2021-08-12 15:53:17 +0545

    Final News App

---
+++ lib/Screens/category_news_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import 'package:lets_learn/models/category_news_model.dart';
+
+import 'package:lets_learn/helper/category_news.dart';
+
+import 'home _screen.dart';
+
+class CategoryNews extends StatefulWidget {
+  final String category;
+  CategoryNews({required this.category});
+  @override
+  _CategoryNewsState createState() => _CategoryNewsState();
+}
+
+class _CategoryNewsState extends State<CategoryNews> {
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'News',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              'App',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w600,
+                fontSize: 18.0,
+              ),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          Opacity(
+            opacity: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Icon((Icons.save)),
+            ),
+          ),
+        ],
+        centerTitle: true,
+        elevation: 1.0 / 2,
+      ),
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 8.0),
+                child: FutureBuilder<CategoryNewsModel?>(
+                  future: CategoryNewsClass().getCategoryNews(widget.category),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.articles!.length,
+                          itemBuilder: (context, index) {
+                            var article = snapshot.data!.articles![index];
+                            return BlogTile(
+                              title: article.title,
+                              description: article.description,
+                              imageUrl: article.urlToImage,
+                              url: article.url,
+                            );
+                          });
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/helper/category_news.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:lets_learn/models/category_news_model.dart';
+import 'package:http/http.dart' as http;
+
+class CategoryNewsClass {
+  Future<CategoryNewsModel?> getCategoryNews(String category) async {
+    var client = http.Client();
+    var categoryNewsModel;
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=us&category=$category&apiKey=732251517fdf4998ab4fb7850917de46";
+    try {
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        var jsonMap = jsonDecode(jsonString);
+        categoryNewsModel = CategoryNewsModel.fromJson(jsonMap);
+        return categoryNewsModel;
+      } else {
+        print(response.statusCode);
+      }
+    } catch (e) {
+      print(e);
+    }
+    return categoryNewsModel;
+  }
+}
